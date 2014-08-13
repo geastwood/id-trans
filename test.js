9cd4d24287099675e6c995f1372eb1eeca88af2a
Fei Liu <liu.feiwood@gmail.com>
2014-08-13 17:59:32 +0200

    very basic

---
+++ test.js
@@ -0,0 +1,67 @@
+Ext.define("IA.pixelcarrier.view.grid.Log", {
+    extend: 'IA.pixelcarrier.view.grid.Base',
+
+    tr: {
+        logUser: 'User',
+        logLastChange: 'Last<br/>Change',
+        logChanges: 'Changes',
+        logChangeTooltipStr: "data-qtip='{0}'",
+        logStatus: 'Status',
+        logUserFormatted: '{0} {1} ({2})',
+        logChangesFormated: '"{0}" changed from "{1}" to "{2}"',
+        logChangesNoChangeMsg: 'There is no change made yet.',
+        emptyRecordMsg: 'No records.'
+    },
+
+    initComponent: function(args) {
+        var me = this;
+
+        me.callParent(args);
+
+        /* jshint expr: true */
+        me.loadData && me.loadData();
+    },
+
+    getColumns: function() {
+        var me = this;
+        return {
+            items: [{
+                header: me.tr.logUser,
+                dataIndex: 'userId',
+                renderer: function(value, meta, record) { // concat use data to `firstName lastName (userId)`
+                    return Ext.String.format(me.tr.logUserFormatted, record.get('firstName'), record.get('lastName'), value);
+                }
+            }, {
+                header: me.tr.logLastChange,
+                dataIndex: 'lastChange'
+            }, {
+                header: me.tr.logChanges,
+                dataIndex: 'changes',
+
+                // concat all the changes and attach the tooltip
+                renderer: function(value, meta) {
+                    var data = Ext.JSON.decode(value), msgStr;
+
+                    if (data && data.length) {
+                        msgStr = Ext.Array.map(data, function(item) {
+                            return Ext.String.format(me.tr.logChangesFormated, item.column, item.oldValue, item.newValue);
+                        }).join('<br/>');
+                    } else {
+                        msgStr = me.tr.logChangesNoChangeMsg;
+                    }
+
+                    // add the tooltip and strip out `null`
+                    meta.tdAttr = Ext.String.format(me.tr.logChangeTooltipStr, msgStr.replace(/[\u0000]/g, ''));
+
+                    return msgStr;
+                }
+            }, {
+                header: me.tr.logStatus,
+                dataIndex: 'status'
+            }],
+            defaults: {
+                flex: 1
+            }
+        };
+    }
+});
--- tr.js
+++ tr.js
@@ -1,2 +1,33 @@
 #!/usr/bin/env node
 console.log('this is the entry point for tr');
+/**
+ * description
+ * if there is no tr, rst[2] will be null
+ */
+var fs = require('fs');
+//var trReg = /define\((?:'|")(.+[^'])(?:'|")[\S\s]*tr:\s?{([\S\s]*?)},/;
+var trReg = /define\((?:'|")(.+[^'])(?:'|")(:?[\S\s]*tr:\s?{([\S\s]*?)},)?/;
+function getTrs(str) {
+    var rst;
+
+    if (typeof str === 'undefined') {
+        return [];
+    }
+
+    var strArray = str.trim().split(/\n/).map(function(line) {
+        return line.trim().replace(/,$/, '');
+    });
+
+    console.log(typeof strArray);
+    return rst;
+}
+/*
+fs.readFile('test.js', 'utf-8', function(err, data) {
+    var rst = trReg.exec(data);
+    getTrs(rst[2]);
+});
+*/
+fs.readFile('test1.js', 'utf-8', function(err, data) {
+    var rst = trReg.exec(data);
+    getTrs(rst[2]);
+});
